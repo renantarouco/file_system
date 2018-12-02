a40d9e69a51875f7e6f89be9cf7969c3268dc7b4
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2018-12-02 15:44:30 -0200

    commit inicial

---
+++ build/CMakeFiles/file_system.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/file_system.dir/src/main.cpp.o"
+  "file_system.pdb"
+  "file_system"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/file_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    cout << "hello, file_system" << endl;
+    return 0;
+}
