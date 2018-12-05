80d8d58122f313ec169a8c88cb28813b76824047
Renan Tarouco da Fonseca <renantarouco@gmail.com>
2018-12-05 11:52:27 -0200

    block manager started.

--- include/block_manager/block_manager.h
+++ include/block_manager/block_manager.h
@@ -1,3 +1,9 @@
-class BlockManager {
+#include <vector>
 
+class BlockManager {
+private:
+    std::vector<bool> _bitmap;
+public:
+    BlockManager(int);
+    std::vector<int> get_available_blocks(int);
 };
---
+++ src/block_manager/block_manager.cpp
@@ -0,0 +1,17 @@
+#include <block_manager/block_manager.h>
+
+BlockManager::BlockManager(int n) : _bitmap(n, false) {
+
+}
+
+std::vector<int> BlockManager::get_available_blocks(int n) {
+    std::vector<int> available_ids;
+    for (int i = 0; i < _bitmap.size() && n > 0; ++i) {
+        if (!_bitmap[i]) {
+            available_ids.push_back(i);
+            n--;
+        }
+    }
+    if (!n) return available_ids;
+    return std::vector<int>();
+}
